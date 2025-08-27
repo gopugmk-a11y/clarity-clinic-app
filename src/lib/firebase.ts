da72769b07a25f5f7bd74e6cc8972219bcfe7439
gopu mohan <gopugmk@gmail.com>
2025-08-27 09:44:09 +0000

    const firebaseConfig = {     apiKey: "AIzaSyDMJfqwdO17KOL-4V0LrWb1_Zxh71

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID",
-  measurementId: "REPLACE_WITH_YOUR_MEASUREMENT_ID"
+    apiKey: "AIzaSyDMJfqwdO17KOL-4V0LrWb1_Zxh71zMO8I",
+    authDomain: "clarityclinic-961eb.firebaseapp.com",
+    projectId: "clarityclinic-961eb",
+    storageBucket: "clarityclinic-961eb.appspot.com",
+    messagingSenderId: "227618481237",
+    appId: "1:227618481237:web:0302007bd007990ad760dc",
+    measurementId: "G-0C3XEGX1T5"
 };
 
 // Initialize Firebase
